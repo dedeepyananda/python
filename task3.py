0ed466001fc3d1b40fd3ed14dabb5a85cfef85d7
dedeepyananda <nanda.dedeepya@gmail.com>
2026-05-16 00:38:34 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,65 @@
+#voting
+"""
+age=int(input("enter age"))
+if age<18:
+    print("not eligible for vote")
+else:
+    print("eligible for vote")
+"""
+#even or odd
+"""
+n=int(input("enter number"))
+if n%2==0:
+    print("n is even number",n)
+else:
+    print("n is odd number",n)
+"""
+#leap year
+"""
+n=int(input("enter year"))
+if n%4==0:
+    print(" n is leap year",n)
+else:
+    print("n is not leap year",n)
+"""
+#vowels 
+"""
+n=input("enter a letter")
+a=["a","e","i","o","u"]
+if n in a:
+    print("vowel")
+else:
+    print("consonant")
+"""
+#greetings
+"""
+a=input("enter name")
+b="dedeepya"
+if a in b:
+    print("welcome",a)
+else:
+    print("welcome guest")
+"""
+"""
+name=input("enter name")
+if name=="dedeepya":
+    print("welcome",name)
+else:
+    print("welcome guest")
+"""
+"""
+n=input("enter name")
+name=["dedeepya","aakanksha","raji","hyma","lakshmi"]
+if n in name:
+    print("welcome ",n)
+else:
+    print("welcome guest")
+"""
+"""
+n=input("enter name").lower()
+name=["dedeepya","aakanksha","raji","hyma","lakshmi"]
+if n in name:
+    print("welcome ",n)
+else:
+    print("welcome guest")
+"""
