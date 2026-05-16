a7f862efee8b81fad6ac21484cbaada0394c42d3
dedeepyananda <nanda.dedeepya@gmail.com>
2026-05-16 19:40:11 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,76 @@
+#using nested if
+#social media
+"""
+user=input("enter user name")
+password=input("enter password")
+if user=="dedhi":
+    if password==1234:
+        print("login successful")
+else:
+    print("invalid")
+"""
+"""
+user=input("enter user name")
+password=input("enter password")
+if user=="dedhi":
+    if password==1234:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("invalid")
+"""
+"""
+user=input("enter user name")
+password=input("enter password")
+if user=="dedhi" and password==1234:
+    print("login successful")
+else:
+    print("invalid")
+"""
+"""
+user=input("enter user name")
+password=input("enter password")
+if user=="dedhi" and password=="dedhi@1234":
+    print("login successful")
+else:
+    print("invalid")
+"""
+"""
+a="dedhi"
+b=1234
+user=input("enter user")
+pass=input("enter pass")
+if user==a:
+    if password==b:
+        print("success")
+"""
+"""#bakery
+cake=input("enter cake name").lower()
+if cake=="red velvet":
+    print("1000 rupees")
+elif cake=="choco almond":
+    print("800 rupees")
+elif cake=="butter scotch":
+    print("600 rupees")
+elif cake=="chocolate cake":
+    print("400 rupees")
+else:
+    print("sorry cake is not available")
+"""
+#pizza
+"""
+price=int(input("enter price"))
+if price==1000:
+    print("BBQ pizza")
+elif price==800:
+    print("crispy chicken pizza")
+elif price==600:
+    print("panner pizza")
+elif price==400:
+    print("corn pizza")
+elif price==200:
+    print("french fries and coke")
+else:
+    print(f"no item for {price} rupees")
+"""   
